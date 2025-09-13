2f684aeaae1eb629f507d91719b28632ad615251
maianoolmos <olmos.martin.mariano@gmail.com>
2025-09-13 13:21:56 +0200

    LICENSE and header

---
+++ src/rtl/risc_v2_memory.vhd
@@ -0,0 +1,41 @@
+--====================================================================
+--
+--   /$$$$$$$  /$$$$$$  /$$$$$$   /$$$$$$        /$$    /$$  /$$$$$$ 
+--  | $$__  $$|_  $$_/ /$$__  $$ /$$__  $$      | $$   | $$ /$$__  $$
+--  | $$  \ $$  | $$  | $$  \__/| $$  \__/      | $$   | $$|__/  \ $$
+--  | $$$$$$$/  | $$  |  $$$$$$ | $$            |  $$ / $$/  /$$$$$$/
+--  | $$__  $$  | $$   \____  $$| $$             \  $$ $$/  /$$____/ 
+--  | $$  \ $$  | $$   /$$  \ $$| $$    $$        \  $$$/  | $$      
+--  | $$  | $$ /$$$$$$|  $$$$$$/|  $$$$$$/         \  $/   | $$$$$$$$
+--  |__/  |__/|______/ \______/  \______/           \_/    |________/
+--
+-- Module:       RISC_V2_MEMORY
+-- Description:  
+--
+-- Author:       Mariano Olmos Martin 
+-- Mail  :       mariano.olmos@outlook.com
+-- Date:         13/9/2025
+-- Version:      v0.0
+-- License: MIT License
+-- 
+-- Copyright (c) 2025 Mariano Olmos
+--
+-- Permission is hereby granted, free of charge, to any person obtaining
+-- a copy of this VHDL code and associated documentation files (the 
+-- "Software"), to deal in the Software without restriction, including 
+-- without limitation the rights to use, copy, modify, merge, publish, 
+-- distribute, sublicense, and/or sell copies of the Software, and to 
+-- permit persons to whom the Software is furnished to do so, subject 
+-- to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be 
+-- included in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY 
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--======================================================================
