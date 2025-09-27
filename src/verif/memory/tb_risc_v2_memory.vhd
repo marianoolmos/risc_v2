c6481acfa7e0f657d43bf84716e82acb3ca52cc0
maianoolmos <olmos.martin.mariano@gmail.com>
2025-09-27 11:35:33 +0200

    1 clock instead of 2

--- src/verif/memory/tb_risc_v2_memory.vhd
+++ src/verif/memory/tb_risc_v2_memory.vhd
@@ -56,7 +56,7 @@ end entity tb_ram_memory;
 
 architecture bench of tb_ram_memory is
 
-  signal clk_a, clk_b : std_logic;
+  signal clk : std_logic;
   -- Ports
 
   signal s_if_instr_i : t_dp_in;
@@ -69,20 +69,14 @@ architecture bench of tb_ram_memory is
 
 begin
 
-  CreateClock(clk_a, 20 ns);
-  CreateClock(clk_b, 20 ns);
+  CreateClock(clk, 20 ns);
 
   ram_memory_inst : entity work.ram_memory
-    generic map (
-      g_mem_width  => C_MEM_WIDTH,
-      g_addr_width => C_ADDR_WIDTH
-    )
     port map (
-      clk_a    => clk_a,
+      clk      => clk,
       port_a_i => s_if_instr_i,
       port_a_o => s_if_instr_o,
 
-      clk_b    => clk_b,
       port_b_i => s_if_data_i,
       port_b_o => s_if_data_o
     );
@@ -90,19 +84,21 @@ begin
   process is
 
     variable vdata_in : std_logic_vector(31 downto 0);
-    variable tmp :unsigned((C_MEM_WIDTH/8)-1  downto 0):=(others => '1') ;
+    variable tmp      : unsigned(C_NUM_BYTES - 1  downto 0) := (others => '1');
+
   begin
 
     sb <= NewID("DP_SB");
     SetTestName("WRITE-READ");
     -- BYTE ENABLE
-    for j in 0 to (C_MEM_WIDTH/8)-1 loop
+    for j in 0 to C_NUM_BYTES - 1 loop
+
       -- ADDR A read B
-      for i in 0 to ((2 ** C_ADDR_WIDTH) - 1) loop
+      for i in 0 to C_MEM_DEPTH loop
 
         s_if_instr_i.en   <= '1';
         s_if_instr_i.we   <= '1';
-        tmp:= (shift_left(to_unsigned(1, tmp), j));
+        tmp               := (shift_left(to_unsigned(1, tmp), j));
         s_if_instr_i.be   <= std_logic_vector(tmp);
         s_if_instr_i.addr <= std_logic_vector(to_unsigned(i, C_ADDR_WIDTH));
         s_if_instr_i.di   <= std_logic_vector(to_unsigned(i, C_MEM_WIDTH));
@@ -120,13 +116,14 @@ begin
         Check(sb, s_if_data_o.do(8 * j + 7 downto 8 * j));
 
       end loop;
-      --ADDR B READ A
-      for i in 0 to ((2 ** C_ADDR_WIDTH) - 1) loop
+
+      -- ADDR B READ A
+      for i in 0 to C_MEM_DEPTH loop
 
         s_if_data_i.en    <= '1';
         s_if_data_i.we    <= '1';
-        tmp:= (shift_left(to_unsigned(1, tmp), j));
-        s_if_data_i.be   <= std_logic_vector(tmp);
+        tmp               := (shift_left(to_unsigned(1, tmp), j));
+        s_if_data_i.be    <= std_logic_vector(tmp);
         s_if_data_i.addr  <= std_logic_vector(to_unsigned(i, C_ADDR_WIDTH));
         s_if_data_i.di    <= std_logic_vector(to_unsigned(i, C_MEM_WIDTH));
         vdata_in          :=std_logic_vector(to_unsigned(i, C_MEM_WIDTH));
@@ -143,6 +140,7 @@ begin
         Check(sb, s_if_instr_o.do(8 * j + 7 downto 8 * j));
 
       end loop;
+
     end loop;
 
     ReportAlerts;
