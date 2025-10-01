1365a2d6b736b73e7a890ab4d93f4207c3fd61db
maianoolmos <olmos.martin.mariano@gmail.com>
2025-10-01 19:24:31 +0200

    alu corretion

--- src/rtl/risc_v2_alu.vhd
+++ src/rtl/risc_v2_alu.vhd
@@ -1,125 +1,132 @@
 --====================================================================
 --
---   /@@@@@@@  /@@@@@@  /@@@@@@   /@@@@@@        /@@    /@@  /@@@@@@ 
+--   /@@@@@@@  /@@@@@@  /@@@@@@   /@@@@@@        /@@    /@@  /@@@@@@
 --  | @@__  @@|_  @@_/ /@@__  @@ /@@__  @@      | @@   | @@ /@@__  @@
 --  | @@  \ @@  | @@  | @@  \__/| @@  \__/      | @@   | @@|__/  \ @@
 --  | @@@@@@@/  | @@  |  @@@@@@ | @@            |  @@ / @@/  /@@@@@@/
---  | @@__  @@  | @@   \____  @@| @@             \  @@ @@/  /@@____/ 
---  | @@  \ @@  | @@   /@@  \ @@| @@    @@        \  @@@/  | @@      
+--  | @@__  @@  | @@   \____  @@| @@             \  @@ @@/  /@@____/
+--  | @@  \ @@  | @@   /@@  \ @@| @@    @@        \  @@@/  | @@
 --  | @@  | @@ /@@@@@@|  @@@@@@/|  @@@@@@/         \  @/   | @@@@@@@@
 --  |__/  |__/|______/ \______/  \______/           \_/    |________/
 --
 -- Module:       RISC_V2_ALU
--- Description:  
+-- Description:
 --
--- Author:       Mariano Olmos Martin 
+-- Author:       Mariano Olmos Martin
 -- Mail  :       mariano.olmos@outlook.com
 -- Date:         13/9/2025
 -- Version:      v0.0
 -- License: MIT License
--- 
+--
 -- Copyright (c) 2025 Mariano Olmos
 --
 -- Permission is hereby granted, free of charge, to any person obtaining
--- a copy of this VHDL code and associated documentation files (the 
--- "Software"), to deal in the Software without restriction, including 
--- without limitation the rights to use, copy, modify, merge, publish, 
--- distribute, sublicense, and/or sell copies of the Software, and to 
--- permit persons to whom the Software is furnished to do so, subject 
+-- a copy of this VHDL code and associated documentation files (the
+-- "Software"), to deal in the Software without restriction, including
+-- without limitation the rights to use, copy, modify, merge, publish,
+-- distribute, sublicense, and/or sell copies of the Software, and to
+-- permit persons to whom the Software is furnished to do so, subject
 -- to the following conditions:
 --
--- The above copyright notice and this permission notice shall be 
+-- The above copyright notice and this permission notice shall be
 -- included in all copies or substantial portions of the Software.
 --
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
--- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
--- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
--- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY 
--- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
--- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 -- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --======================================================================
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
-  use ieee.math_real.all;
-
   use work.risc_v2_pkg.all;
+  use work.risc_v2_isa_pkg.all;
 
-  entity risc_v2_alu is
-    port (
-        clk   : in std_logic;
-        rs1,rs2   : in    std_logic_vector(31 downto 0);
-        imm   : in    std_logic_vector(31 downto 0);
-        rsi   : in    std_logic_vector(11 downto 0);
-        rd    : out    std_logic_vector(31 downto 0);
-        reset : in std_logic
-        
-    );
-  end entity;
+entity risc_v2_alu is
+  port (
+    CLK      : in  std_logic;
+    RESET    : in  std_logic;
+    ALU_OP   : in  std_logic_vector(3 downto 0);
+    RS1      : in  std_logic_vector(C_REG_WIDTH-1 downto 0);
+    RS2      : in  std_logic_vector(C_REG_WIDTH-1 downto 0);
+    O_RESULT : out std_logic_vector(C_REG_WIDTH-1 downto 0);
+    O_EQ     : out std_logic;
+    O_LT     : out std_logic;
+    O_LTU    : out std_logic
+  );
+end entity risc_v2_alu;
 
-  architecture rtl of risc_v2_alu is
+architecture rtl of risc_v2_alu is
   
+  signal result : std_logic_vector(C_REG_WIDTH-1 downto 0);  
+begin
+
+
+  
+  process(RS1, RS2, ALU_OP)
+    variable v_sub : signed(C_REG_WIDTH downto 0);
   begin
+    
+
+    if (ALU_OP = ALU_SUB or ALU_OP = ALU_SLT or ALU_OP = ALU_SLTU) then
+      v_sub := signed('0' & RS1) - signed('0' & RS2);
+    else
+      v_sub := (others => '0');  
+    end if;
+    
+    case ALU_OP is
+      
+      when ALU_ADD =>
+        result <= std_logic_vector(signed(RS1) + signed(RS2));
+      
+      when ALU_SUB =>
+        result <= std_logic_vector(v_sub(C_REG_WIDTH-1 downto 0));
+
+        O_EQ  <= '1' when v_sub = 0 else '0'; 
+        O_LT  <= v_sub(C_REG_WIDTH);
+        O_LTU <= RS1(C_REG_WIDTH-1) xor RS2(C_REG_WIDTH-1) xor v_sub(C_REG_WIDTH-1);
+      
+      when ALU_SLT =>
+        result <= (0 => v_sub(C_REG_WIDTH), others => '0');
+      
+      when ALU_SLTU =>
+        result <= (0 => (RS1(C_REG_WIDTH-1) xor RS2(C_REG_WIDTH-1) xor 
+                        v_sub(C_REG_WIDTH-1)), others => '0');
+      
+      when ALU_XOR =>
+        result <= RS1 xor RS2;
+      
+      when ALU_OR =>
+        result <= RS1 or RS2;
+      
+      when ALU_AND =>
+        result <= RS1 and RS2;
+      
+      when ALU_SLL =>
+        result <= std_logic_vector(shift_left(unsigned(RS1), to_integer(unsigned(RS2(4 downto 0)))));
+      
+      when ALU_SRL =>
+        result <= std_logic_vector(shift_right(unsigned(RS1), to_integer(unsigned(RS2(4 downto 0)))));
+      
+      when ALU_SRA =>
+        result <= std_logic_vector(shift_right(signed(RS1), to_integer(unsigned(RS2(4 downto 0)))));
+      
+      when others =>
+        result <= (others => '0');
+        
+    end case;
+    
+  end process;
+  process (CLK)
+  begin
+    if rising_edge(clk) then
+      O_RESULT <= result;
+    end if;
+    
+  end process;
   
-    process (clk)
-    begin
-      if rising_edge(clk) then
-        case ins is
-          when c_add =>
-            rd <= rs1 + rs2;
-          when c_sub =>
-            rd <= rs1 - rs2;
-          when c_xor =>
-            rd <= rs1 xor rs2;
-          when c_or =>
-            rd <= rs1 or rs2;
-          when c_and =>
-            rd <= rs1 and rs2;
-          when c_sll =>
-            rd <= shift_left(rs1, rs2);
-          when c_srl =>
-            rd <= shift_right(rs1, rs2);
-          when c_sra =>
-            rd <= rs1 xor rs2;
-          when c_slt =>
-            rd <= rs1 xor rs2;
-          when c_sltu =>
-            rd <= rs1 xor rs2;
-          when others =>
-            null;
-        end case;
-      end if;
-    end process;
-  
-    process (clk)
-    begin
-      if rising_edge(clk) then
-        case insi is
-          when c_addi =>
-            rd <= rs1 + imm;
-          when c_subi =>
-            rd <= rs1 - imm;
-          when c_xori =>
-            rd <= rs1 xor imm;
-          when c_ori =>
-            rd <= rs1 or imm;
-          when c_andi =>
-            rd <= rs1 and imm;
-          when c_slli =>
-            rd <= shift_left(rs1, imm);
-          when c_srl =>
-            rd <= shift_right(rs1, imm);
-          when c_srai =>
-            rd <= rs1 xor imm;
-          when c_slti =>
-            rd <= rs1 xor imm;
-          when c_sltui =>
-            rd <= rs1 xor imm;
-          when others =>
-            null;
-        end case;
-      end if;
-    end process;
-  
-  end architecture;
+
+end architecture rtl;
